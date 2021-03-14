d38ba959a4c4810146076f9591911e6e4996f25e
Ricardo Costa <ricardo_cas4@hotmail.com>
2021-03-13 23:35:32 -0300

    ajustando o escopo global do arquiv

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,7 @@
     const conteudo = `<p class="content">${valor}</p>`;
     tarefa.innerHTML = conteudo;
     tarefa.appendChild(BotaoConclui());
+    tarefa.appendChild(BotaoDeleta());
     lista.appendChild(tarefa);
     input.value = "";
   };
@@ -34,5 +35,24 @@
     const tarefaCompleta = botaoConclui.parentElement;
     // toggle devolve um booleano se o botao foi clicado ou nao
     tarefaCompleta.classList.toggle("done");
+  };
+
+  // criando um componente do botão deletar tarefa
+
+  const BotaoDeleta = () => {
+    const botaoDeleta = document.createElement("button");
+    botaoDeleta.classList.add("delete-button");
+    botaoDeleta.innerText = "deletar";
+    botaoDeleta.addEventListener("click", deletarTarefa);
+    return botaoDeleta;
+  };
+
+  const deletarTarefa = (evento) => {
+    const botaoDeleta = evento.target;
+    // quando eu deletar, quero deletar o elemento e não o botao
+    const tarefaCompleta = botaoDeleta.parentElement;
+    // removendo o elemento
+    tarefaCompleta.remove();
+    return botaoDeleta;
   };
 })();
