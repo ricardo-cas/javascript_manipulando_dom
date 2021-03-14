f68d505649801a93a14846bda28ac3ae5903b7d8
Ricardo Costa <ricardo_cas4@hotmail.com>
2021-03-13 22:11:39 -0300

    pegando o valor que é digitado no campo e armazenando dentro de uma variável

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,8 @@
 const novaTarefa = document.querySelector("[data-form-button]");
 
 novaTarefa.addEventListener("click", () => {
+  const input = document.querySelector("[data-form-input]");
+  const valor = input.value;
   console.log("fui clicado :)");
+  console.log(valor);
 });
