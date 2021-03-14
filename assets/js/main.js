61a5e8be2d90a86f69dedd8d529132cc04983b7d
Ricardo Costa <ricardo_cas4@hotmail.com>
2021-03-13 23:17:07 -0300

    terminado a funcionalidade do botao concluir tarefa.

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,6 +31,6 @@ const concluirTarefa = (evento) => {
   const botaoConclui = evento.target;
   // subindo um elemento na DOM, ou seja, pegando o elemento pai do botao que eu cliquei.
   const tarefaCompleta = botaoConclui.parentElement;
-
+  // toggle devolve um booleano se o botao foi clicado ou nao
   tarefaCompleta.classList.toggle("done");
 };
