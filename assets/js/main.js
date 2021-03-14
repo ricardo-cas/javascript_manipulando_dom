90e939b9bae0bccf494af4561abf3aa68d6741df
Ricardo Costa <ricardo_cas4@hotmail.com>
2021-03-13 22:22:13 -0300

    prevenindo que o input envie dados para algum lugar e a pagina seja recarregada

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,10 @@
 const novaTarefa = document.querySelector("[data-form-button]");
 
-novaTarefa.addEventListener("click", () => {
+novaTarefa.addEventListener("click", (evento) => {
+  evento.preventDefault();
   const input = document.querySelector("[data-form-input]");
   const valor = input.value;
   console.log("fui clicado :)");
   console.log(valor);
+  input.value = "";
 });
